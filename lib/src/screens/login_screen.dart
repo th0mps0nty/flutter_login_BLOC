f403ba945ff24b9f23018e0849fa0211fb87865e
Tyler Thompson <th0mps0n.ty@protonmail.com>
2020-11-22 00:29:57 -0500

    get to same UI as previous app

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      onPressed: () {},
+      child: Text('Login'),
+      color: Colors.green,
+    );
+  }
+}
